36c7733376f8a650c60ccfb32ef80524f88d8ece
Bhargav Shekhar <70209326+BhargavShekhar@users.noreply.github.com>
2023-10-17 11:11:49 +0530

    Update LG.bat
    fixed message console

--- Love game/LG.bat
+++ Love game/LG.bat
@@ -1,7 +1,9 @@
 @echo off
+
 title ----------------------------------------------- I LOVE YOU -----------------------------------------------
+echo x = msgbox("I LOVE YOU !!", 48, "Welcome to the LOVE GAME") >> msgbox.vbs
+start msgbox.vbs
 :AYU
 color 0c
 echo This is a Love virius
-msg * I Love You !
 goto AYU
